6e451f33a73f0d232937d7aa924fa03df6c3065a
abitwiseman <abitwiseman@gmail.com>
2022-02-16 14:16:27 +0530

    add cube to scene

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-function fuckKamal() {
-    document.getElementById('try').innerHTML = "Fuck u Kamal!!!"
+// console.log(THREE)
+
+// Scene
+const scene = new THREE.Scene()
+
+// Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: '#f00' })
+const cube = new THREE.Mesh(geometry, material)
+
+scene.add(cube)
+
+
+const sizes = {
+    width: 800,
+    height: 600
 }
+
+
+//Camera : POV
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) //vertical fov, AR
+camera.position.z = 3
+camera.position.x = 2
+camera.position.y = 1
+scene.add(camera)
+
+// Renderer
+const canvas = document.getElementsByClassName('webgl')[0]
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
